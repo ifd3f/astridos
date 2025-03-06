5a95a7d49e6c3bd6e5447161e3bdd1d2c917e226
Astrid Yu <astrid@astrid.tech>
2025-03-06 08:08:49 -0800

    theoretically this will say hello world?

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,9 +4,18 @@
 #[cfg(not(test))]
 mod panic_handler;
 
+const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    const HELLO: &[u8] = b"Hello World!";
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *VGA_BUFFER.offset(i as isize * 2) = byte;
+            *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
-
-fn main() {}
