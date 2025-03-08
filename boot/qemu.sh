499a15f8526c1d61996772b96659cf60de51b726
Astrid Yu <astrid@astrid.tech>
2025-03-07 16:26:42 -0800

    make the qemu run helper work from clean

--- boot/qemu.sh
+++ boot/qemu.sh
@@ -2,11 +2,20 @@
 
 set -euxo pipefail
 
+# set up an esp
 cargo build --target x86_64-unknown-uefi --release
-rm -r esp
+rm -rf esp
 mkdir -p esp/efi/boot
 cp -r target/x86_64-unknown-uefi/release/astridos-bootos.efi esp/efi/boot/bootx64.efi
-qemu-system-x86_64 -enable-kvm \
+
+# copy ovmfies in
+ovmfdir=$(nix build .#debug.boot.ovmf.fd --print-out-paths --no-link)
+rm -f *.fd
+cp $ovmfdir/FV/* .
+chmod +rw *.fd
+
+# launch!
+qemu-system-x86_64 \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
     -drive format=raw,file=fat:rw:esp
